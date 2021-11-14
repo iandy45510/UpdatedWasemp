e5c05b59b979160f4388208a209ba9f8ce84ea01
RJCP21 <90582393+RJCP21@users.noreply.github.com>
2021-11-14 21:24:07 +0800

    backtraking

--- WASEMP/Admin/Backtracking.php
+++ WASEMP/Admin/Backtracking.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>BackTracking</title>
+    <title>Blank Page - Brand</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
     <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
@@ -30,78 +30,61 @@
                     <div class="sidebar-brand-icon rotate-n-15"><i class="fas fa-church"></i></div>
                     <div class="sidebar-brand-text mx-3"><span>Wasemp</span></div>
                 </a>
-                
+
                 <hr class="sidebar-divider my-0">
                 <ul class="navbar-nav text-light" id="accordionSidebar">
                     <li class="nav-item">
-                      <a class="nav-link active" href="#"><i class="fa fa-user"></i><span>Account</span></a>
-                      <a class="nav-link active" href="#"><i class="fas fa-server"></i><span>List Of Server</span></a>
-                      <a class="nav-link active" href="#"><i class="fas fa-chart-pie"></i><span>Analytics</span></a>
-                      <a class="nav-link active" href="#"><i class="fas fa-qrcode"></i><span>QR</span></a>
-                      <a class="nav-link active" href="#"><i class="fas fa-location-arrow"></i><span>BackTracking</span></a>
-                      <a class="nav-link active" href="#" style="text-shadow: 0px 0px;"><i class="fas fa-sms"></i><span style="height: 0px;">SMS</span></a>
-                      <a class="nav-link active" href="#" style="margin: 0px;"><i class="fas fa-bug"></i><span>Report</span></a>
-                      <a class="nav-link active" href="#" style="margin: 0px;"><i class="fas fa-sign-out-alt"></i><span>Logout</span></a></li>
+                        <a class="nav-link" href=""><i class="fa fa-user"></i><span>Account</span></a>
+                        <a class="nav-link" href=""><i class="fas fa-server"></i><span>List Of Server</span></a>
+                        <a class="nav-link" href=""><i class="fas fa-chart-pie"></i><span>Analytics</span></a>
+                        <a class="nav-link" href=""><i class="fas fa-qrcode"></i><span>QR</span></a>
+                        <a class="nav-link" href=""><i class="fas fa-location-arrow"></i><span>BackTracking</span></a>
+                        <a class="nav-link" href="" style="text-shadow: 0px 0px;"><i class="fas fa-sms"></i><span style="height: 0px;">SMS</span></a>
+                        <a class="nav-link" href="" style="margin: 0px;"><i class="fas fa-bug"></i><span>Report</span></a>
+                        <a class="nav-link" href="" style="margin: 0px;"><i class="fas fa-sign-out-alt"></i><span>Logout</span>
+                        </a>
+                    </li>
                 </ul>
                 <!-- media -->
                 <div class="text-center d-none d-md-inline"><button class="btn rounded-circle border-0" id="sidebarToggle" type="button" style="padding-left: 12px;"></button></div>
-                <footer class="footer-basic" style="background: var(--bs-dark);height: 210px;">
-                    <div class="social" style="background: var(--bs-dark);margin-bottom: 0px;margin-top: 8px;">
-                      <a href="#" style="background: var(--bs-white);border-color: var(--bs-dark);"><i class="icon ion-social-instagram"></i></a>
-                      <a href="#" style="background: var(--bs-white);"><i class="icon ion-social-youtube"></i></a>
-                      <a href="#" style="background: var(--bs-white);"><i class="icon ion-social-facebook"></i></a></div>
+                <footer class="footer-basic" style="background: var(--bs-dark);">
+                    <div class="social" style="background: var(--bs-dark);padding-right: 0px;padding-top: 0px;margin-top: 0px;margin-bottom: 0px;margin-right: 0px;margin-left: 0px;font-size: 34px;">
+                        <a href="https://www.instagram.com/kielmoreno92" style="background: var(--bs-white);border-color: var(--bs-dark);"><i class="icon ion-social-instagram"></i></a>
+                        <a href="https://www.youtube.com/channel/UCc4RpGJxaIY0fFI27WhASug" style="background: var(--bs-white);"><i class="icon ion-social-youtube"></i></a>
+                        <a href="https://www.facebook.com/SanExequielMorenoParish" style="background: var(--bs-white);"><i class="icon ion-social-facebook"></i>
+                        </a>
+                    </div>
                 </footer>
                 <footer class="footer-dark" style="background: var(--bs-dark);transform: rotate(-91deg);padding: 0px 0px;"></footer>
             </div>
         </nav>
-        <!-- navbar &table -->
-        <div class="d-flex flex-column" id="content-wrapper">
-            <div id="content">
-                <nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
+
+        <div class="d-flex flex-column" id="content-wrapper" style="margin-top: 0px;padding-top: 0px;margin-right: 0px;margin-bottom: 0px;padding-bottom: 0px;">
+            <div id="content" style="margin-right: 0px;padding-right: 0px;padding-left: 10px;">
+                <nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top" style="padding-left: 0px;padding-right: 0px;margin-right: 0px;margin-left: -9px;">
                     <div class="container-fluid"><button class="btn btn-link d-md-none rounded-circle me-3" id="sidebarToggleTop" type="button"><i class="fas fa-bars"></i></button></div>
-                </nav><div class="form-group pull-right">
-    <input type="text" class="search form-control" placeholder="What you looking for?">
-</div>
-<span class="counter pull-right"></span>
-<table class="table table-hover table-bordered results">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th class="col-md-5 col-xs-5">Name / Surname</th>
-      <th class="col-md-4 col-xs-4">Job</th>
-      <th class="col-md-3 col-xs-3">City</th>
-    </tr>
-    <tr class="warning no-result">
-      <td colspan="4"><i class="fa fa-warning"></i> No result</td>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Balázs Barta</td>
-      <td>UI & UX</td>
-      <td>Eger</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Dániel Nagy</td>
-      <td>Graphic Designer</td>
-      <td>Eger</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Szilárd Sebők</td>
-      <td>Software Developer</td>
-      <td>Budapest</td>
-    </tr>
-    <tr>
-      <th scope="row">4</th>
-      <td>Sándor Fekete</td>
-      <td>Front-end Developer</td>
-      <td>Luxemburg</td>
-    </tr>
-  </tbody>
-</table>
+                </nav>
+                <!-- table -->
+                <div class="container" style="padding-top: 74px;"><table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+        <thead>
+            <tr>
+                 <th>Name</th>
+                <th>Date & Time</th>
+                <th>Place</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Luffy</td>
+                <td>10/10/21-10:30</td>
+                <td>SM</td>
+               
+            </tr>
+            <tr>
+              
+            </tr>
+        </tbody>
+    </table></div>
             </div>
         </div>
     </div>
